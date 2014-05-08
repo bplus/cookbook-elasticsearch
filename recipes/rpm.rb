294ec0063df92efec9d27adab57b89ec1b22cd7e
Bryan Nelson <bryan.nelson@gmail.com>
2014-05-08 15:53:08 -0500

    bringing in elasticsearch.yml and Erubis to the rpm recipe

--- recipes/rpm.rb
+++ recipes/rpm.rb
@@ -1,5 +1,8 @@
 # See <http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/_linux.html>
 
+[Chef::Recipe, Chef::Resource].each { |l| l.send :include, ::Extensions }
+Erubis::Context.send(:include, Extensions::Templates)
+
 filename = node.elasticsearch[:rpm_url].split('/').last
 
 remote_file "#{Chef::Config[:file_cache_path]}/#{filename}" do
@@ -10,4 +13,14 @@ end
 
 rpm_package "#{Chef::Config[:file_cache_path]}/#{filename}" do
   action :install
+end
+
+# Create ES config file
+#
+template "elasticsearch.yml" do
+  path   "#{node.elasticsearch[:path][:conf]}/elasticsearch.yml"
+  source "elasticsearch.yml.erb"
+  owner node.elasticsearch[:user] and group node.elasticsearch[:user] and mode 0755
+
+  notifies :restart, 'service[elasticsearch]' unless node.elasticsearch[:skip_restart]
 end
